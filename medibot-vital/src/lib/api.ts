85796225e6efb47d3cb40939001ef7fd5fe08ab7
kavya-agrawal22 <kavya22j@gmail.com>
2026-04-17 18:25:31 +0530

    Update api.ts

--- medibot-vital/src/lib/api.ts
+++ medibot-vital/src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // FIX: Explicitly append /api so it matches your Spring Boot paths
-  @RequestMapping                             
+                              
   baseURL: import.meta.env.PROD 
     ? "https://mediconnectbackend-47oq.onrender.com/api" 
     : "/api",
